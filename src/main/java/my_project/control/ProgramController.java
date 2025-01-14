ab47c7866ed9beb883485b019f3a30a5e9bf9e1c
vikigro <gromvikt@kkg-do.eu>
2025-01-14 13:14:40 +0100

    yay

--- src/main/java/my_project/control/ProgramController.java
+++ src/main/java/my_project/control/ProgramController.java
@@ -6,6 +6,7 @@ import my_project.Config;
 import my_project.model.Apple;
 import my_project.model.Pear;
 import my_project.model.Player;
+import my_project.model.Traube;
 
 /**
  * Ein Objekt der Klasse ProgramController dient dazu das Programm zu steuern. Die updateProgram - Methode wird
@@ -21,6 +22,7 @@ public class ProgramController {
 
     private Apple apple01;
     private Pear pear01;
+    private Traube traube01;
     private Player player01;
 
     /**
@@ -42,7 +44,9 @@ public class ProgramController {
         double xPos = Math.random()*(Config.WINDOW_WIDTH-50) + 50;
         double yPos = Math.random()*(Config.WINDOW_HEIGHT-50) + 50;
         apple01 = new Apple(xPos, yPos);
+        traube01 = new Traube(xPos, yPos);
         viewController.draw(apple01);
+        viewController.draw(traube01);
 
         xPos = Math.random()*(Config.WINDOW_WIDTH-50) + 50;
         yPos = Math.random()*(Config.WINDOW_HEIGHT-50) + 50;
@@ -61,21 +65,18 @@ public class ProgramController {
     public void updateProgram(double dt){
         //TODO 08 Nachdem Sie die TODOs 01-07 erledigt haben: Setzen Sie um, dass im Falle einer Kollision (siehe TODO 06 bzw. 07) zwischen dem Spieler und dem Apfel bzw. dem Spieler und der Birne, die jumpBack()-Methode von dem Apfel bzw. der Birne aufgerufen wird.
         //Weitere TODOs folgen und werden im Unterricht formuliert. Spätestens nach TODO 08 sollte der Aufbau des Projekts durchdacht werden.
-        if(checkAndHandleCollision(apple01) == true){
+        if(checkAndHandleCollision(apple01)){
             apple01.jumpBack();
         }
-        if(checkAndHandleCollision(pear01) == true) {
+        if(checkAndHandleCollision(pear01)) {
             pear01.jumpBack();
         }
     }
 
     //TODO 06 Fügen Sie eine Methode checkAndHandleCollision(Apple a) hinzu. Diese gibt true zurück, falls das Apple-Objekt mit dem Player-Objekt kollidiert. Nutzen Sie hierzu die collidesWith-Methode der Klasse GraphicalObject.
     public boolean checkAndHandleCollision(GraphicalObject g0){
-        if(g0.collidesWith(player01)) {
-            return true;
-        }else{
-            return false;
-        }
+        return g0.collidesWith(player01);
+        //die funktion hier oben ist schon entweder true oder false, deshalb ist es irrelevant ein if statement daraus zu machen.
     }
     //TODO 07 Fügen Sie eine Methode checkAndHandleCollision(Pear p) hinzu. Diese gibt true zurück, falls das Pear-Objekt mit dem Player-Objekt kollidiert. Nutzen Sie hierzu die collidesWith-Methode der Klasse GraphicalObject.
 }
---
+++ src/main/java/my_project/model/Traube.java
@@ -0,0 +1,38 @@
+package my_project.model;
+
+import KAGO_framework.model.GraphicalObject;
+import KAGO_framework.view.DrawTool;
+
+public class Traube extends GraphicalObject {
+
+    //Attribute
+    private double speed;
+
+    public Traube(double x, double y){
+        this.x = x;
+        this.y = y;
+        speed = 150;
+        radius = 10;
+    }
+
+    @Override
+    public void draw(DrawTool drawTool) {
+        drawTool.setCurrentColor(160,0,255,255);
+        drawTool.drawFilledCircle(x,y,radius);
+        drawTool.setCurrentColor(0,0,0,255);
+        drawTool.drawCircle(x,y,radius);
+    }
+
+    @Override
+    public void update(double dt) {
+        y = y + speed * dt;
+        if (y >= 1000 + radius) {
+            jumpBack();
+        }
+    }
+
+    public void jumpBack() {
+        y = 0 - radius;
+        x = Math.random() * (1000 - 2 * radius) + radius;
+    }
+}
